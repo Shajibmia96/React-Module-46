514da76708cc7f9e7a0ff678a95ee45ff105a129
Shajib96 <shajibahamedornob@gmail.com>
2024-03-27 16:17:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+   const handlesSubmitted = (e) =>{
+        e.preventDefault();
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+        console.log(e.target.Phone.value)
+       console.log("Submitted your documents")
+   }
+
+  return (
+    <>
+       <h1>Please Enter your E-mail here</h1>
+
+       <form  onSubmit={handlesSubmitted}>
+          <input type="text" name="name" id="" placeholder='Name' className='border-2 border-red-400' /> <br />
+          <input type="text" name="email" id="" placeholder='E-mail' className='border-2 border-red-400' /> <br />
+          <input type="text" name="Phone" id="" placeholder='Phone' className='border-2 border-red-400' /> <br />
+          <input type="submit" value="Submit" className='border-2 border-blue-400' />
+       </form>
+    </>
+  )
+}
+
+export default App
